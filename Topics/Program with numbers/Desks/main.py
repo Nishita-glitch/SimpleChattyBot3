696c5dfa53052e1b222205e82c18f25f6515109e
Nishita-glitch <Nishita.raje@online.island.edu.hk>
2022-01-12 09:19:04 +0800

    Initial commit

---
+++ Topics/Program with numbers/Desks/main.py
@@ -0,0 +1,7 @@
+numstudents1 = int(input(""))
+desks1 = (numstudents1 // 2) + (numstudents1 % 2)
+numstudents2 = int(input(""))
+desks2 = (numstudents2 // 2) + (numstudents2 % 2)
+numstudents3 = int(input(""))
+desks3 = (numstudents3 // 2) + (numstudents3 % 2)
+print(desks1 + desks2 + desks3)
---
+++ Topics/Program with numbers/How many nuts will be left after division/main.py
@@ -0,0 +1,4 @@
+nuts = float(input())
+squirrels = float(input())
+nuts_left = nuts % squirrels
+print("nuts_left")
